a095f04106c4de887cfee5a4b386eb443bbbcf51
Guy Beaven <guy@clingstone.com.au>
2026-08-22 19:59:30 +1000

    feat: formatted currency with commas, link titles to AusTender

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+
+  eleventyConfig.addFilter("formatMoney", function(value) {
+    if (!value) return "";
+    return "$" + Math.round(value).toLocaleString("en-AU");
+  });
+
   return {
     dir: {
       input: "src",
